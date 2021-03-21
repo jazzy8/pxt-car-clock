81c83f2be148b83146d992002c5d3d99448093b1
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-03-20 23:01:31 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,32 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    serial.writeLine("cur::00,1")
+    if (receivedNumber > -45 && receivedNumber < 45) {
+        serial.writeLine("shw::North")
+    } else if (receivedNumber > -135 && receivedNumber < -45) {
+        serial.writeLine("shw::West")
+    } else if (receivedNumber > -225 && receivedNumber < -135) {
+        serial.writeLine("shw::South")
+    } else {
+        serial.writeLine("shw::East")
+    }
+    basic.pause(325)
+    serial.writeLine("cur::00,1")
+    basic.pause(100)
+    serial.writeLine("shw::" + receivedNumber)
+    basic.pause(325)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "calib") {
+        serial.writeLine("cls::all")
+    }
+})
 serial.redirect(
 SerialPin.P14,
 SerialPin.P15,
 BaudRate.BaudRate115200
 )
+basic.pause(200)
+serial.writeLine("shw::--")
+basic.pause(325)
+serial.writeLine("cur::00,1")
+serial.writeLine("shw::Calibrating")
